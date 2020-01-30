cf6bb36863d4354ab7d59ec8ea9cc047f4430aa5
s1ovak <45860242+s1ovak@users.noreply.github.com>
2020-01-30 10:10:25 +0300

    init commit

---
+++ bin/custom/training/trainingbackoffice/gensrc/org/trainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 28 янв. 2020 г., 12:44:58                   ---
+ * ----------------------------------------------------------------
+ */
+package org.trainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ bin/custom/training/trainingfulfilmentprocess/gensrc/org/training/fulfilmentprocess/constants/GeneratedTrainingFulfilmentProcessConstants.java
@@ -0,0 +1,40 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 28 янв. 2020 г., 12:44:58                   ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package org.training.fulfilmentprocess.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "trainingfulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedTrainingFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}

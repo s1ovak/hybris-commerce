0c5628d1d16e3dd23ffeedbf24417fc92a89e08e
s1ovak <45860242+s1ovak@users.noreply.github.com>
2020-01-30 14:49:06 +0300

    Generated new addon 'questions'.

---
+++ bin/custom/questions/gensrc/org/training/constants/GeneratedQuestionsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 30 янв. 2020 г., 14:34:34                   ---
+ * ----------------------------------------------------------------
+ */
+package org.training.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedQuestionsConstants
+{
+	public static final String EXTENSIONNAME = "questions";
+	
+	protected GeneratedQuestionsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
